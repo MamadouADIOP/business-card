a86e138dc80a63e477ea2814af8d892fce92fe34
Mamadou Abdoulaye DIOP <mdiop@teksystems.com>
2024-07-31 14:30:11 -0400

    Refactor code

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,12 +19,11 @@ import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -41,7 +39,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(modifier = Modifier.fillMaxSize()) {
                     BusinessCard("Jennifer Doe", phoneNumber = "jen.doe@android.com", feedbackPath = "@Android Dev", email = "jen.doe@android.com", position = "Android Developer Extraordinaire", modifier = Modifier)
                 }
             }
@@ -50,19 +48,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun IdentitySection(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun IdentitySection(name: String, position: String, modifier: Modifier = Modifier) {
+
+        Column(modifier = Modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = stringResource(R.string.logo_android_text),
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Black)
+            )
+            Text(
+                text = name,
+                modifier = modifier,
+                fontSize = 30.sp
+            )
+            Text(
+                text = position,
+                modifier = modifier,
+                fontWeight =  FontWeight.Bold,
+                color = Color(0xFF023020)
+            )
+        }
+  }
 
 @Composable
-fun ContactSection(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactSection(phoneNumber: String, feedbackPath: String, email: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(bottom = 60.dp, start = 6.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.Start) {
+        Row {
+            Icon(Icons.Rounded.Call, contentDescription = stringResource(R.string.phone_number_talkback), modifier= Modifier)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = phoneNumber,
+                modifier = modifier.padding(bottom = 15.dp)
+            )
+        }
+        Row {
+            Icon(Icons.Rounded.Share, contentDescription = stringResource(R.string.feedback_hash_tag_talkback), modifier= Modifier)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = feedbackPath,
+                modifier = modifier.padding(bottom = 15.dp)
+            )
+        }
+        Row {
+            Icon(Icons.Rounded.Email, contentDescription = stringResource(R.string.email_address_talkback), modifier= Modifier)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = email,
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Composable
@@ -72,59 +109,15 @@ fun BusinessCard(name: String, phoneNumber: String, feedbackPath: String, email:
             Modifier
                 .weight(1f)
                 .fillMaxSize(), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
-            Column(modifier = Modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(
-                    painter = painterResource(id = R.drawable.android_logo),
-                    contentDescription = stringResource(R.string.logo_android_text),
-                    modifier = Modifier
-                        .size(100.dp)
-                        .background(Color.Black)
-                )
-                Text(
-                    text = "$name",
-                    modifier = modifier,
-                    fontSize = 30.sp
-                )
-                Text(
-                    text = "$position",
-                    modifier = modifier,
-                    fontWeight =  FontWeight.Bold,
-                    color = Color(0xFF023020)
-                )
-            }
+            IdentitySection(name = name, position = position, modifier = Modifier)
         }
         Row (
             Modifier
                 .weight(1f)
                 .fillMaxSize(), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
-            Column(modifier = Modifier.padding(bottom = 60.dp, start = 6.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.Start) {
-                Row {
-                    Icon(Icons.Rounded.Call, contentDescription = stringResource(R.string.phone_number_talkback), modifier= Modifier)
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(
-                        text = "$phoneNumber",
-                        modifier = modifier.padding(bottom = 15.dp)
-                    )
-                }
-                Row {
-                    Icon(Icons.Rounded.Share, contentDescription = stringResource(R.string.feedback_hash_tag_talkback), modifier= Modifier)
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(
-                        text = "$feedbackPath",
-                        modifier = modifier.padding(bottom = 15.dp)
-                    )
-                }
-                Row {
-                    Icon(Icons.Rounded.Email, contentDescription = stringResource(R.string.email_address_talkback), modifier= Modifier)
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(
-                        text = "$email",
-                        modifier = modifier
-                    )
-                }
+            ContactSection(phoneNumber, feedbackPath, email, Modifier)
             }
         }
-    }
 }
 
 @Preview(showBackground = true)
